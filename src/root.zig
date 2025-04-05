7a2c446ae2a120e0e7cd5b29c6e7e4792d1766e1
floppyMike <36990990+floppyMike@users.noreply.github.com>
2025-04-05 15:35:55 +0200

    Add stringmatching bruteforce

--- src/root.zig
+++ src/root.zig
@@ -2,6 +2,7 @@ const std = @import("std");
 
 pub const cli = @import("cli.zig");
 pub const csv = @import("csv.zig");
+pub const smt = @import("stringmatching.zig");
 
 test {
     std.testing.refAllDecls(@This());
