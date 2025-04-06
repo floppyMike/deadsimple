14ea5a0e9e4af43431484b19ca26fabc0b6f7fbc
floppyMike <36990990+floppyMike@users.noreply.github.com>
2025-04-06 16:56:08 +0200

    Add stringmatching boyer moore

--- src/stringmatching.zig
+++ src/stringmatching.zig
@@ -97,6 +97,96 @@ pub fn knuthMorrisPratt(
     return null;
 }
 
+pub fn boyerMooreAlloc(alloc: std.mem.Allocator, haystack: []const u8, needle: []const u8) !?usize {
+    const pt = try alloc.alloc(isize, (needle.len + 1) * 2);
+    defer alloc.free(pt);
+    return boyerMoore(haystack, .{
+        .len = needle.len,
+        .needle = needle.ptr,
+        .suffix_table = pt[0..(needle.len + 1)].ptr,
+        .shift_table = pt[(needle.len + 1)..].ptr,
+    });
+}
+
+pub fn boyerMoore(
+    haystack: []const u8,
+    needle_data: struct {
+        /// Length of the needle.
+        len: usize,
+        /// String to search with (size: len)
+        needle: [*]const u8,
+        /// Memory for the algorithm to use for the suffix table (size: len + 1)
+        suffix_table: [*]isize,
+        /// Memory for the algorithm to use for the shift table (size: len + 1)
+        shift_table: [*]isize,
+    },
+) ?usize {
+    const needle = needle_data.needle;
+    const suffix = needle_data.suffix_table;
+    const shift = needle_data.shift_table;
+
+    const needle_len: isize = @intCast(needle_data.len);
+    const haystack_len: isize = @intCast(haystack.len);
+
+    if (needle_len > haystack.len) return null;
+
+    // Calculate bad table
+    const bad_table = blk: {
+        var bad_table: [1 << (@sizeOf(u8) * 8)]isize = undefined;
+        @memset(&bad_table, -1);
+
+        for (0..@intCast(needle_len)) |i| {
+            const ch = needle[i];
+            bad_table[ch] = @intCast(i);
+        }
+
+        break :blk bad_table;
+    };
+
+    // Calculate suffix/shift table step 1
+    {
+        @memset(shift[0..@intCast(needle_len + 1)], 0);
+
+        var i = needle_len;
+        var j = needle_len + 1;
+        suffix[@intCast(i)] = j;
+
+        while (i > 0) {
+            while (j <= needle_len and needle[@intCast(i - 1)] != needle[@intCast(j - 1)]) {
+                if (shift[@intCast(j)] == 0) shift[@intCast(j)] = j - i;
+                j = suffix[@intCast(j)];
+            }
+
+            i -= 1;
+            j -= 1;
+
+            suffix[@intCast(i)] = j;
+        }
+    }
+
+    // Calculate suffix/shift table step 2
+    {
+        var j = suffix[0];
+        for (0..@intCast(needle_len + 1)) |i| {
+            if (shift[@intCast(i)] == 0) shift[@intCast(i)] = j;
+            if (i == j) j = suffix[@intCast(j)];
+        }
+    }
+
+    // Evalutate haystack
+    {
+        var i: isize = 0;
+        var j: isize = undefined;
+        while (i <= haystack_len - needle_len) {
+            j = needle_len - 1;
+            while (j >= 0 and needle[@intCast(j)] == haystack[@intCast(i + j)]) j -= 1;
+            if (j < 0) return @intCast(i) else i += @max(shift[@intCast(j + 1)], j - bad_table[haystack[@intCast(i + j)]]);
+        }
+    }
+
+    return null;
+}
+
 test "Fuzz string matching" {
     std.testing.log_level = .info;
 
@@ -119,5 +209,6 @@ test "Fuzz string matching" {
 
         try std.testing.expectEqual(expect, bruteForce(&haystack, needle));
         try std.testing.expectEqual(expect, try knuthMorrisPrattAlloc(std.testing.allocator, &haystack, needle));
+        try std.testing.expectEqual(expect, try boyerMooreAlloc(std.testing.allocator, &haystack, needle));
     }
 }
